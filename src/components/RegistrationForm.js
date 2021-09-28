e8805816a92f5826bf8bf1f974ee21e6f13a116b
elcinp <elcinpars1@gmail.com>
2021-09-28 22:15:47 +0300

    required part added

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid, Paper, Button, Typography, TextField } from "@material-ui/core";
-import {Formik,Form,Field} from 'formik'
+import {Formik,Form,Field,ErrorMessage} from 'formik'
 import * as Yup from 'yup'
 
 const RegistrationForm = () => {
@@ -15,7 +15,10 @@ const RegistrationForm = () => {
   }
 
   const validationSchema=Yup.object().shape({
-    name:Yup.string().required("Required")
+    name:Yup.string().required("Required"),
+    email:Yup.string().email("Enter valid email").required("Required"),
+    phoneNumber:Yup.string().typeError('Enter valid Phone Number').required('Required'),
+    password:Yup.string().required('Required')
   })
     
   
@@ -38,10 +41,12 @@ const RegistrationForm = () => {
             {(props)=>(
                 <Form>
                   {console.log(props)}
-                <TextField label="Name" name="name"fullWidth value={props.values.name} onChange={props.handleChange}/>
-                <TextField label="Email" name="email"type="email" fullWidth {...props.getFieldProps('email')}/>
-                <Field as={TextField} name='phoneNumber'label="Phone Number" fullWidth />
-                <Field as={TextField} name='password' label='Password' fullWidth/>
+                {/* <TextField label="Name" name="name"fullWidth value={props.values.name} onChange={props.handleChange} helperText={<ErrorMessage name='name'/>}/> */}
+                <Field as={TextField} name='name'label="Name" fullWidth helperText={<ErrorMessage name='name'/>} />
+                {/* <TextField label="Email" name="email"type="email" fullWidth {...props.getFieldProps('email')}/> */}
+                <Field as={TextField} name='email'label="Email" fullWidth helperText={<ErrorMessage name='email'/>} />
+                <Field as={TextField} name='phoneNumber'label="Phone Number" fullWidth helperText={<ErrorMessage name='phoneNumber'/>} />
+                <Field as={TextField} name='password' label='Password' fullWidth helperText={<ErrorMessage name='password'/>}/>
                 <Button
                   type="submit"
                   style={btnStyle}
